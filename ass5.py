fcc1cd28c90c50bd51841b8b95ba922caae44638
avinashselvam <avinashpanneer@gmail.com>
2018-10-20 16:12:36 +0530

    6th added

--- ass5.py
+++ ass5.py
@@ -1,5 +1,7 @@
+from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 def f1(x):
     return x ** 2
@@ -12,31 +14,53 @@ def der_h1(x, h, f=f1):
     backward = (f(x) - f(x - h)) / h
     return [forward, backward]
 
-def q1():
-    xs = np.arange(-5, 5, 0.1)
-    h4s = []
-    for h in [0.5, 0.25, 0.2, 0.1, 0.01]:
-        h4, hf, hb = [], [], []
+# def q1():
+#     xs = np.arange(-5, 5, 0.1)
+#     h4s = []
+#     for h in [0.5, 0.25, 0.2, 0.1, 0.01]:
+#         h4, hf, hb = [], [], []
         
-        for x in xs:
-            h4.append(der_h4(x, h))
-            h1 = der_h1(x, h)
-            hf.append(h1[0])
-            hb.append(h1[1])
+#         for x in xs:
+#             h4.append(der_h4(x, h))
+#             h1 = der_h1(x, h)
+#             hf.append(h1[0])
+#             hb.append(h1[1])
         
-        # comparing h4, forward, backeard for different h
-        plt.plot(xs, h4); plt.plot(xs, hf); plt.plot(xs, hb)
-        plt.title("comparision")
-        plt.gca().legend(("h4", "h1 forward", "h1 backward"))
-        plt.xlabel("x"); plt.ylabel("derivative"); plt.show()
+#         # comparing h4, forward, backeard for different h
+#         plt.plot(xs, h4); plt.plot(xs, hf); plt.plot(xs, hb)
+#         plt.title("comparision")
+#         plt.gca().legend(("h4", "h1 forward", "h1 backward"))
+#         plt.xlabel("x"); plt.ylabel("derivative"); plt.show()
 
-        h4s.append(h4)
+#         h4s.append(h4)
+
+#     # comparing different h
+#     for i in range(5): plt.plot(xs, h4s[i])
+#     plt.title("different h")
+#     plt.gca().legend(('0.5', '0.25', '0.2', '0.1', '0.01'))
+#     plt.xlabel("x"); plt.ylabel("derivative"); plt.show()
+
+def q1():
+    x = 2
+    hs = []
+    df4 = []
+    dff = []
+    dfb = []
+    h = 1
+    while h > 0.0001:
+        hs.append(np.log(h))
+        df4.append(der_h4(x, h))
+        temp = der_h1(x, h)
+        dff.append(temp[0])
+        dfb.append(temp[1])
+        h /= 2
 
     # comparing different h
-    for i in range(5): plt.plot(xs, h4s[i])
-    plt.title("different h")
-    plt.gca().legend(('0.5', '0.25', '0.2', '0.1', '0.01'))
-    plt.xlabel("x"); plt.ylabel("derivative"); plt.show()
+    plt.plot(hs, df4); plt.plot(hs, dff); plt.plot(hs, dfb); 
+    plt.title("derivative vs log h")
+    plt.gca().legend(('O(h^4)', 'forward O(h)', 'backward O(h)'))
+    plt.xlabel("log h"); plt.ylabel("derivative"); plt.show()
+
         
 def f2(x):
     return x**2
@@ -71,17 +95,26 @@ def gauss_3(a, b, f=f2):
     I = (5*f(t(-x0)) + 5*f(t(x0)) + 8*f(t(0)))/9
     return I * (c / 2)
 
-def adaptive(a, b):
-    i = 0
-    converged = False
-    prev = 0
-    while not converged:
-        h = (b - a) / (2**i)
-        I = simpson(a, b, h)
-        converged = abs(I - prev) < 0.0001
-        prev = I
-    return I
+def adaptive(a, b, epsilon=2, i=0, f=f2):
+    i += 1
+    c = (a + b) / 2
+
+    I = (f(a) + f(b) + 4 * f((b + a) / 2))*((b-a)/6)
+    if i > 10 : return I
+    I_adap = (f(a) + 4 * f((a+c)/2) + 2*f(c) + 4*f((b+c)/2) + f(b))*((b-a)/12)
+    
+    converged = abs(I - I_adap) < epsilon
+
+    if not converged:
+        I_adap = adaptive(a, c, epsilon/2, i) + adaptive(c, b, epsilon/2, i)
+    
+    return I_adap
 
 def q2():
     for fn in [trapezoid, simpson, gauss_2, gauss_3, adaptive]:
+        start = time.time()
         print(fn(0, 2))
+        end = time.time()
+        print("time taken for {} : {}".format(str(fn) ,end-start))
+
+q2()
